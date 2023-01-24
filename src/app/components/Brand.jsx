c7e7340d6588b136b24f61465ca4733a0824c12b
Yusuf Boran <yusuf.boran@rbbt.com.tr>
2023-01-24 10:13:25 +0300

    icon href '/'

--- src/app/components/Brand.jsx
+++ src/app/components/Brand.jsx
@@ -1,19 +1,20 @@
-import { Box, styled } from '@mui/material';
-import { MatxLogo } from 'app/components';
-import useSettings from 'app/hooks/useSettings';
-import { Span } from './Typography';
+import { Box, styled } from "@mui/material";
+import { MatxLogo } from "app/components";
+import useSettings from "app/hooks/useSettings";
+import { NavLink } from "react-router-dom";
+import { Span } from "./Typography";
 
 const BrandRoot = styled(Box)(() => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: '20px 18px 20px 29px',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: "20px 18px 20px 29px",
 }));
 
 const StyledSpan = styled(Span)(({ mode }) => ({
   fontSize: 18,
-  marginLeft: '.5rem',
-  display: mode === 'compact' ? 'none' : 'block',
+  marginLeft: ".5rem",
+  display: mode === "compact" ? "none" : "block",
 }));
 
 const Brand = ({ children }) => {
@@ -23,14 +24,18 @@ const Brand = ({ children }) => {
 
   return (
     <BrandRoot>
-      <Box display="flex" alignItems="center">
-        <MatxLogo />
-        <StyledSpan mode={mode} className="sidenavHoverShow">
-          MielProje
-        </StyledSpan>
-      </Box>
-
-      <Box className="sidenavHoverShow" sx={{ display: mode === 'compact' ? 'none' : 'block' }}>
+      <NavLink to={"/"}>
+        <Box display="flex" alignItems="center">
+          <MatxLogo />
+          <StyledSpan mode={mode} className="sidenavHoverShow">
+            MielProje
+          </StyledSpan>
+        </Box>
+      </NavLink>
+      <Box
+        className="sidenavHoverShow"
+        sx={{ display: mode === "compact" ? "none" : "block" }}
+      >
         {children || null}
       </Box>
     </BrandRoot>
